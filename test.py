2beb345e9b1340bd9e06c3c85e8d9f75c28c427d
Tengis1003 <wgretyon@gmail.com>
2025-05-30 14:38:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,131 @@
-print("hi")
+<!DOCTYPE html>
+<html lang="eng">
+  <head>
+    <link rel="stylesheet" href="style.css" />
+    <style>
+      #Course {
+        font-weight: bold;
+      }
+      #tahoma {
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: bold;
+      }
+      #pro {
+        font-weight: 1;
+        line-height: 0.1;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: small;
+      }
+      #statement {
+        font-weight: 300;
+        line-height: 2;
+      }
+      #deal {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: large;
+        font-weight: bold;
+      }
+      #sale {
+        margin-top: -20px;
+        font-size: small;
+        color: red;
+      }
+      #shop {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: medium;
+        font-weight: bold;
+        /* text-align: center; /
+      }
+      #see {
+        font-size: small;
+        font-weight: 100;
+        line-height: 0;
+        / text-align: center; */
+      }
+      #shop-container {
+        width: 300px;
+        text-align: center;
+      }
+      #apple {
+        text-align: center;
+        width: 300px;
+        line-height: 1;
+        margin-top: -10px;
+      }
+      #new {
+        color: orange;
+        font-size: small;
+      }
+      #mac {
+        font-weight: bold;
+        font-size: medium;
+      }
+      #super {
+        font-size: large;
+        font-weight: bold;
+      }
+      #price {
+        font-size: small;
+        font-weight: 10;
+      }
+      #usd {
+      }
+      #a {
+        display: inline;
+        font-size: large;
+        font-weight: bold;
+      }
+      #b {
+        display: inline;
+        font-size: small;
+        font-weight: 10;
+      }
+      #c {
+        color: green;
+        font-size: small;
+      }
+      #d {
+        font-size: small;
+        display: inline;
+        font-weight: 1;
+      }
+      #e {
+        color: red;
+        display: inline;
+        font-size: small;
+      }
+    </style>
+  </head>
+  <body>
+    <p id="tahoma">This is Tahoma Font</p>
+    <br />
+    <p id="Course">HTMl CSS Course</p>
+    <p id="pro">Beginner to Pro</p>
+    <p id="statement">
+      In this course, we'll learn the skills you need to become a developer.
+    </p>
+    <button id="start">Get Started</button> <br />
+    <p id="deal">Biggest Deal of the Year!</p>
+    <p id="sale">Sales end Tuesday</p>
+    <br />
+    <div id="shop-container">
+      <p id="shop">Shopping for your bsuiness?</p>
+      <p id="see">See how Apple at Work can help.</p>
+    </div>
+    <br />
+    <div id="apple">
+      <p id="new">New</p>
+      <p id="mac">MacBook Pro</p>
+      <p id="super">Supercharged for pros.</p>
+      <p id="price">From $1999</p>
+      <button id="buy">Buy</button>
+    </div>
+    <div id="usd">
+      <p id="a">1,049.61</p>
+      <p id="b">USD</p>
+      <p id="c">+18.05(1.75)today</p>
+      <p id="d">After hours 1,048.00</p>
+      <p id="e">-1.61(0.15%)</p>
+    </div>
+  </body>
+</html>
